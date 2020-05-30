be3ceff4e9c6414984c5e8d1d3511f1488d422d8
William Perron <thewilliamperron@gmail.com>
2020-05-30 12:31:20 -0400

    added benchmark for problem #33

--- leetcode/033_test.go
+++ leetcode/033_test.go
@@ -12,3 +12,11 @@ func TestSearch(t *testing.T) {
 		t.Errorf("Error: expected position to be 4 for input %v and target %d, got %d\n", list, target, pos)
 	}
 }
+
+func BenchmarkSearch(b *testing.B) {
+	list := []int{4, 5, 6, 7, 0, 1, 2}
+	target := 0
+	for i := 0; i < b.N; i++ {
+		search(list, target)
+	}
+}
